a87f40eb4d708d43c2e2b767fdaca2b43b7a85fc
AbhishekS04 <abhishek23main@gmail.com>
2025-02-09 22:12:08 +0530

    add main.py file with variable examples and print statements

---
+++ BroCodeYt/main.py
@@ -0,0 +1,45 @@
+# variable = a container for a value (String , Integer , Float, Boolean) 
+# a variable behave as if it was the value it contains 
+
+
+#! Strings
+# first_name="Abhishek"
+# last_name="Singh"
+# food="pizza"
+# email="Abhishek23main@gmail.com"
+
+# print(f"hello {  first_name} {last_name}")
+# print(f"Your favorite food is {food}")
+# print(f"Your email is {email}")
+
+#! Integer
+# age=21
+# quantity=10
+# num_of_students=100
+# print(f"your age is {age}")
+# print(f"Quantity is {quantity}")
+# print(f"Number of students is {num_of_students}")
+
+#! Float
+
+# price=1000.0
+# gpa=7.29
+# print(f"Price is {price}")
+# print(f"Your GPA is {gpa}")  
+
+#! Boolean
+
+is_student=False
+for_sale=True
+# print(f"Are you a student? {is_student}")  
+
+# if is_student:
+#     print("You are a student") 
+# else:
+#     print("You are not a student")
+
+if for_sale:
+    print("The item is for sale")
+else:
+    print("The item is not for sale")
+    
