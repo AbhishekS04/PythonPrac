635b31699fa38dafb422b067be6c20223cd9feed
AbhishekS04 <abhishek23main@gmail.com>
2025-02-09 22:16:32 +0530

    enhance typecasting.py with detailed explanation and variable examples

--- BroCodeYt/typecasting.py
+++ BroCodeYt/typecasting.py
@@ -1,1 +1,10 @@
-#  Typecasting = the process of converting a variable
+#  Typecasting = the process of converting a variable from one data type to another data type
+
+#  str(),int(),float(),bool()
+
+name = "Abhishek"
+age = 21
+gpa= 7.29
+is_student = True
+
+print(type(name))
