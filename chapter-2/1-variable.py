fc2abe433d586889be7fdaeb512e7681e38027aa
AbhishekS04 <abhishek23main@gmail.com>
2025-02-09 19:58:37 +0530

    refactor variable definitions and add new variable examples in chapter 2

--- chapter-2/1-variable.py
+++ chapter-2/1-variable.py
@@ -1,8 +1,9 @@
-a =10 #! integer
-b=20 #! integer
-name="Abhishek" #! string
-d= False #! boolean
-e=None #! NoneType
+# a =10 #! integer
+# b=20 #! integer
+# name="Abhishek" #! string
+# d= False #! boolean
+# e=None #! NoneType
 
-print("the sum is:", a + b)
-print(name)
+# print("the sum is:", a + b)
+# print(name)
+
---
+++ chapter-2/2-variable.py
@@ -0,0 +1,13 @@
+myvar = "Abhishek"
+my_var = "Abhishek"
+_my_var = "Abhishek"
+myVar = "Abhishek"
+MYVAR = "Abhishek"
+myvar2 = "Abhishek"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
