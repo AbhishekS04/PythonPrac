d54724cd1bbeeb9b4b803363b4dd31b4a2bbea72
AbhishekS04 <abhishek23main@gmail.com>
2025-02-09 11:00:33 +0530

    update comments to enhance clarity in variable definitions in 1-variable.py

--- chapter-2/1-variable.py
+++ chapter-2/1-variable.py
@@ -1,8 +1,8 @@
-a =10 # integer
-b=20 # integer
-name="Abhishek" # string
-d= False # boolean
-e=None # NoneType
+a =10 #! integer
+b=20 #! integer
+name="Abhishek" #! string
+d= False #! boolean
+e=None #! NoneType
 
 print("the sum is:", a + b)
 print(name)
