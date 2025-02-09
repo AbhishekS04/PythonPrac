d2075078f2fd66cc9629b2e6fd8b27ca5eb384a7
AbhishekS04 <abhishek23main@gmail.com>
2025-02-09 22:51:13 +0530

    enhance typecasting.py with additional typecasting examples and print statements

--- BroCodeYt/typecasting.py
+++ BroCodeYt/typecasting.py
@@ -7,4 +7,21 @@ age = 21
 gpa= 7.29
 is_student = True
 
-print(type(name))
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(is_student))
+
+# gpa= int(gpa)
+# print(gpa)
+# gpa=float(gpa)
+# print(gpa)
+
+# age=str(age)
+# print(age)
+
+# age +="1"
+# print(age)
+
+name = bool(name)
+print(name)
