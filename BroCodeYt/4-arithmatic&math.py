e860307105c34f5e2a7045437552b2ca8950e391
AbhishekS04 <abhishek23main@gmail.com>
2025-02-16 22:30:42 +0530

    add: implement area calculation for a circle using math module in 4-arithmatic&math.py

--- BroCodeYt/4-arithmatic&math.py
+++ BroCodeYt/4-arithmatic&math.py
@@ -1,4 +1,4 @@
-friend=2
+# friend=10
 # friend = friend + 1
 # friend +=
 
@@ -11,9 +11,45 @@ friend=2
 # friend = friend / 2
 # friend /= 2
 
+# friend = friend % 2
+# friend  %= 2
 
-print(int(friend))
+# print(friend)
 
 
+# ! built in functions
+
+# x=3.14
+# y=-4
+# z=5
+# print(round(x))
+# print(abs(y))
+# print(pow(z,2))
+# print(max(x,y,z))
+
+#  ! Math module
+
+import math
+
+# x=9.1
+
+# print (math.pi)
+# print(math.e)
+# print (math.sqrt(16))
 
 
+# print(math.floor(x))
+# result = math.ceil(x)
+# result = math.floor(x)
+# print(result)
+
+# radius = input("Enter the radius of the circle: ")
+# radius = float(radius)
+# circumference = 2* math.pi * radius
+# print(round(circumference))
+
+# !
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * math.pow(radius,2)
+print(round(area,2))
