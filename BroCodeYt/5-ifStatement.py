abb0b8b53ae69a8e13ca29ee3656fad48eb4a226
AbhishekS04 <abhishek23main@gmail.com>
2025-02-23 22:37:32 +0530

    refactor: remove age verification logic and related print statements in 5-ifStatement.py

--- BroCodeYt/5-ifStatement.py
+++ BroCodeYt/5-ifStatement.py
@@ -1,11 +1,12 @@
-# !
+# 
 
-age = int ((input("Enter your age: ")))
-if age >= 100:
-    print("You are too old to vote ")
-elif age >=18:
-    print("You are eligible to vote")
-else:
-    print("You are not eligible to vote")
-    
-print("Thank you for checking your eligibility")
+# age = int ((input("Enter your age: ")))
+# if age >= 100:
+#     print("You are too old to vote ")
+# elif age >=18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+# print("Thank you for checking your eligibility")
+
